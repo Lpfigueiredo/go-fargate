b38b592166d275dd0d3c0bb1db05654cca3acfc5
Leonardo Paulo Figueiredo <60974999+Lpfigueiredo@users.noreply.github.com>
2023-06-26 09:38:08 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -146,6 +146,11 @@ resource "aws_iam_role" "ecs_task_role" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "hello_world" {
   family                   = "hello-world-app"
   network_mode             = "awsvpc"
